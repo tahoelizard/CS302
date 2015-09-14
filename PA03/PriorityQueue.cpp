c75230a7413c03f35d0d52469402776c84821b48
Liz <tahoelizard>
2015-09-13 22:44:41 -0700

    enqueue needs sorting. Lot of cleaning to do

--- PA03/PriorityQueue.cpp
+++ PA03/PriorityQueue.cpp
@@ -21,6 +21,9 @@
 // Header files ///////////////////////////////////////////////////////////////
 
 #include "PriorityQueue.h"
+ #include <iostream>
+  using namespace std;
+
 
 /**
  * @brief Default constructor
@@ -36,14 +39,14 @@ template <class DataType>
 PriorityQueue<DataType>::PriorityQueue()
 {
 	head = NULL;
-	rear = NULL; 
 }
 
 // destructor
 template <class DataType>
 PriorityQueue<DataType>::~PriorityQueue( )
 {
-	
+	//delete head; broken
+	head = NULL;
 }
 
 template <class DataType>
@@ -58,30 +61,76 @@ bool PriorityQueue<DataType>::isEmpty()
 }
 
 template <class DataType>
-void PriorityQueue<DataType>::dequeue(DataType)
+void PriorityQueue<DataType>::dequeue(DataType& data)
 {
+
+
+
+	if(!isEmpty()){
+
+		data.priority = (*head)[0].priority;
+		strcpy(data.process, (*head)[0].process);
+
+		for(int i = 0; i < head->getSize(); i++){
+			(*head)[i].priority = (*head)[i+1].priority;
+			strcpy( (*head)[i].process, (*head)[i+1].process );
+		}
+
+		head->decrementSize();
+	}
+	
 
 }
 
 template <class DataType>
 void PriorityQueue<DataType>::enqueue (int newPri, char* newPro)
 {
+		int counter = 0;
+	if(isEmpty()){
+    	head = new SimpleVector<DataType>; 
+    }
 
+   (*head)[head->getSize()].priority = newPri;
+
+   strcpy((*head)[head->getSize()].process, newPro);
+
+
+    head->incrementSize();
+
+    if(head->getSize() >= head->getCapacity()){
+    	head->grow(1);
+    }
 }
 
 template <class DataType>
 void PriorityQueue<DataType>::peekAtFront(DataType &data)
 {
 	if(!isEmpty()){
-		
+
+	}
+	else{
+		//throw exception 
 	}
 }
 
 template <class DataType>
 void PriorityQueue<DataType>::showStructure(char name)
 {
-
+	if(!isEmpty()){
+		cout << name << " (" << head->getSize() << "/" <<head->getCapacity() << "): ";
+		for (int i = 0; i < head->getSize(); i++){
+			cout << "[";
+			cout << (*head)[i].priority << "/";
+			cout << (*head)[i].process << "] ";
+			
+		}
+		cout << endl;
+	}
+	else{
+		cout << "Empty list" << endl;
+	}
 }
+
 
 
 
