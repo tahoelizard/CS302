ad36826af69de181f15218eae10d42c0ef46b895
Liz <tahoelizard>
2015-09-20 13:31:48 -0700

    pre-sort functions done. About to implement sort

--- PA04/TestSorter.cpp
+++ PA04/TestSorter.cpp
@@ -49,7 +49,6 @@ void TestSorter::getValueAt(int value, DateType& valFound )const
 
 bool TestSorter::add(char* newDate)
 {
-  int index = 0; 
   SimpleVector::setValueAt( getSize(), newDate );
   SimpleVector::incrementSize();
   if(SimpleVector::getSize() == SimpleVector::getCapacity())
@@ -58,33 +57,145 @@ bool TestSorter::add(char* newDate)
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
 bool TestSorter::sort()
 {
-  int index = 0; 
+  int index = 1; 
   int firstDate, secondDate; 
-
+  int firstMonth, secondMonth;
+  swapDates(0, 1);
 
   while(index < SimpleVector::getSize()){
-    //turn first two into nums. should be toNum function 
-    
-    cout << "comparing " << " to " << endl; 
+
+    firstDate  = toNumber(index);
+    firstMonth = quantifyMonth(index);
+    cout << firstDate << endl; 
     index++;
+    
   }
   return true;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int TestSorter::toNumber(int index)
 {
     DateType hold;
-    int result = 2;
+    int result;
     getValueAt( index, hold );
     result = hold.date[0]; 
     result = result - 48;
-      if(hold.date[1] != ' ')
+    if(hold.date[1] != ' ')
     {
-      result = result *10; 
+      result = result * 10;
+      result = result + (hold.date[1] - 48);
+   }
+    return result;
+}
 
+int TestSorter::quantifyMonth(int index)
+{
+  int result = 0;
+  int findIndex = 0;
+  DateType hold;
+  getValueAt( index, hold );
+  while (hold.date[findIndex] != ' ')
+  {
+    findIndex++; 
+  }
+  findIndex++; 
+
+  switch ( hold.date[findIndex] ) {
+  case 'j': case 'J':
+    if (hold.date[findIndex+1] == 'a' || hold.date[findIndex+1] == 'A'){
+      result = 1;
     }
+    else if(hold.date[findIndex+1] == 'u' || hold.date[findIndex+1] == 'U'){
+      if(hold.date[findIndex+2] == 'n' || hold.date[findIndex+2] == 'N'){
+        result = 6;
+      }
+      else{
+        result = 7;
+      }
+    }
+  break;
+  case 'f':
+    result = 2; 
+  break;
+  case 'm':
+    if (hold.date[findIndex+2] == 'r' || hold.date[findIndex+2] == 'R'){
+      result = 3;
+    }
+    else{
+      result = 5;
+    }
+  break;
+  case 'a':
+      if (hold.date[findIndex+1] == 'p' || hold.date[findIndex+1] == 'P'){
+      result = 4;
+    }
+    else{
+      result = 8;
+    }
+  break;
+  case 's':
+    result = 9;
+  break;
+  case 'o':
+    result = 10;
+  break;
+  case 'n':
+    result = 11;
+  break;
+  case 'd':
+    result = 12;
+  break;
+default:
+    result = 0;
+  break;
+}
+
+  return result;
+}
+
+void TestSorter::swapDates(int firstIndex, int secondIndex){
+  DateType holdFirst;
+  DateType holdSecond; 
+  getValueAt( firstIndex, holdFirst );
+  getValueAt( secondIndex, holdSecond ); 
+  setValueAt(firstIndex, holdSecond);
+  setValueAt(secondIndex, holdFirst);
 }
 
 #endif // ifndef TESTSORTER_CPP
