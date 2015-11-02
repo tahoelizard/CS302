130efbde366c0ce8c96057b7a3617cc94eda64e6
Liz <tahoelizard>
2015-11-02 12:59:22 -0800

    added actual files

---
+++ 8PA/SolverClass.cpp
@@ -0,0 +1,65 @@
+// Program Information ////////////////////////////////////////////////////////
+/**
+ * @file SolverClass.cpp
+ *
+ * @brief Implementation file for class that attempts to locate smaller
+ *        inside a larger container rectangle
+ * 
+ * @author Elizabeth Johnson
+ * 
+ * @details Specifies all member methods of the Solver class
+ *
+ * @version 1.00 (18 October 2015)
+ *
+ * @Note None
+ */
+
+// Precompiler directives /////////////////////////////////////////////////////
+
+#ifndef SOLVERCLASS_CPP
+#define SOLVERCLASS_CPP
+
+// Header files ///////////////////////////////////////////////////////////////
+
+#include <stdexcept>
+ #include "SolverClass.h"
+
+using namespace std;
+
+// constructor
+SolverClass::SolverClass()
+{
+
+}
+
+// destructor
+SolverClass::~SolverClass( )
+{
+
+}
+
+// modifiers
+bool SolverClass::findSolution()
+{
+
+}
+bool SolverClass::setContainerRectangle( int height, int width )
+{
+
+}
+bool SolverClass::addInsideRectangle( int height, int width )
+{
+
+}
+void SolverClass::setDisplayFlag( bool setFlag )
+{
+
+}
+void SolverClass::displayField() const
+{
+
+}
+
+// Terminating precompiler directives  ////////////////////////////////////////
+
+#endif		// #ifndef SOLVERCLASS_CPP
